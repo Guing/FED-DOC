3a40f552b220a5093f6b2194aac3c8893258a61f
zhengguihong <649925003@qq.com>
2022-03-29 11:52:08 +0800

    edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://guing.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Guing', // Usually your GitHub org/user name.
+  projectName: 'FED-DOC', // Usually your repo name.
 
   presets: [
     [
