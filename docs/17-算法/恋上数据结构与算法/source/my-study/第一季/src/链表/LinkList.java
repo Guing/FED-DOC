c6b04cee437f79c634b1fffef14c0a0cdab7b807
aguing <aguing@outlook.com>
2022-09-13 07:50:46 +0800

    edit

---
+++ docs/17-算法/恋上数据结构与算法/source/my-study/第一季/src/链表/LinkList.java
@@ -0,0 +1,135 @@
+package 链表;
+
+ interface List<E> {
+
+    /**
+     * 清除所有元素
+     */
+    void clear();
+
+    /**
+     * 元素的数量
+     * @return
+     */
+    int size();
+
+    /**
+     * 是否为空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    boolean contains(E element);
+
+    /**
+     * 添加元素到尾部
+     * @param element
+     */
+    void add(E element);
+
+    /**
+     * 获取index位置的元素
+     * @param index
+     * @return
+     */
+    E get(int index);
+
+    /**
+     * 设置index位置的元素
+     * @param index
+     * @param element
+     * @return 原来的元素ֵ
+     */
+    E set(int index, E element);
+
+    /**
+     * 在index位置插入一个元素
+     * @param index
+     * @param element
+     */
+    void add(int index, E element);
+
+    /**
+     * 删除index位置的元素
+     * @param index
+     * @return
+     */
+    E remove(int index);
+
+    /**
+     * 查看元素的索引
+     * @param element
+     * @return
+     */
+    int indexOf(E element);
+}
+
+
+public class LinkList<E> implements List<E> {
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void add(E element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
